cfbce440aecfb309ecb5b539e77c2afa9624b9c1
wow <github@wowow.cloud>
2021-03-29 15:37:13 +0200

    Fix some requests, code cleaning
    Working and usable

--- node-proxmox.js
+++ node-proxmox.js
@@ -1,43 +1,45 @@
-module.exports = function ProxmoxApi(hostname, user, pve, password){
-	// INIT vars
-    this.hostname = hostname;
-    this.user = user;
-    this.password = pve;
-    this.pve = password;
-    this.token = {};
-    this.tokenTimestamp = 0;
+const querystring = require('querystring');
+const http = require('https');
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 
-    function login(hostname, user, pve, password, callback)
-	{		
-		var querystring = require('querystring');
-		body = { password: password, username: user};
+module.exports = function ProxmoxApi(hostname, user, pve, password) {
+	// INIT vars
+	this.hostname = hostname;
+	this.user = user;
+	this.password = pve;
+	this.pve = password;
+	this.token = {};
+	this.tokenTimestamp = 0;
+
+	function login(hostname, user, pve, password, callback) {
+		let body = {password: password, username: user};
 		body = querystring.stringify(body);
-		var headers = {
-			'Content-Type':'application/x-www-form-urlencoded',
+		const headers = {
+			'Content-Type': 'application/x-www-form-urlencoded',
 			'Content-Length': Buffer.byteLength(body)
 		};
-		var options = {
+		const options = {
 			host: hostname,
 			rejectUnauthorized: false, //Allow unauthorized SSL certificate
 			port: 8006,
 			path: '/api2/json/access/ticket',
 			method: 'POST',
-			headers: headers
+			headers: headers,
+			timeout: 120,
 		};
-		var http = require('https');
-		var par = this;
-		var req = http.request(options, function(res){
+		const par = this;
+		const req = http.request(options, function (res) {
 			res.setEncoding('utf8');
-		    var _data = '';
+			let _data = '';
 			res.on('data', function (chunk) {
 				//Error handling to do
 				_data += chunk;
 			});
 			res.on('end', function () {
-				var data = JSON.parse(_data).data;
+				const data = JSON.parse(_data).data;
 				par.token = {ticket: data.ticket, CSRFPreventionToken: data.CSRFPreventionToken};
 				par.tokenTimestamp = new Date().getTime();
-				if (typeof(callback) == 'function')
+				if (typeof (callback) == 'function')
 					callback();
 			});
 		});
@@ -45,88 +47,86 @@ module.exports = function ProxmoxApi(hostname, user, pve, password){
 		req.end();
 	}
 
-	function call(method, url, body, callback)
-	{
-		currentTime = new Date().getTime();
+	function call(method, url, body, callback) {
+		let currentTime = new Date().getTime();
 		//1 hour login timeout
-		if(currentTime - this.tokenTimestamp > 60 * 60 * 1000)
-		{
-			login(this.hostname, this.user, this.password, this.pve, function(){callApi(method, url, body, callback);});
-		}
-		else
-		{
+		if (currentTime - this.tokenTimestamp > 60 * 60 * 1000) {
+			login(this.hostname, this.user, this.password, this.pve, function () {
+				callApi(method, url, body, callback);
+			});
+		} else {
 			callApi(method, url, body, callback);
 		}
 	}
 
-	function callApi(method, url, body, callback)
-	{
-		var currentTime = new Date().getTime();	
-		
-		var querystring = require('querystring');
+	function callApi(method, url, body, callback) {
+		let headers;
+		let currentTime = new Date().getTime();
 
 		//Compute signature
-		if(body == undefined)
+		if (body === undefined)
 			body = '';
 		else
 			body = querystring.stringify(body);
 
-		if(method == 'GET')
-		{
-			var headers = {
-				'Cookie':'PVEAuthCookie='+this.token.ticket
+		if (method === 'GET') {
+			headers = {
+				'Cookie': 'PVEAuthCookie=' + this.token.ticket
 			};
-		}
-		else {
-			var headers = {
-				'Content-Type':'application/x-www-form-urlencoded',
+		} else {
+			headers = {
+				'Content-Type': 'application/x-www-form-urlencoded',
 				'Content-Length': Buffer.byteLength(body),
-				'CSRFPreventionToken':this.token.CSRFPreventionToken,
-				'Cookie':'PVEAuthCookie='+this.token.ticket
+				'CSRFPreventionToken': this.token.CSRFPreventionToken,
+				'Cookie': 'PVEAuthCookie=' + this.token.ticket,
+				'Origin': 'https://' + this.hostname + ':8006',
+				Connection: "keep-alive"
 			};
 		}
 
-		var options = {
+		const options = {
 			host: this.hostname,
 			rejectUnauthorized: false,
 			port: 8006,
-			path: '/api2/json'+url,
+			path: '/api2/json' + url,
 			method: method,
-			headers: headers
+			headers: headers,
+			timeout: 60,
+			gzip: true,
+
 		};
 
-		var http = require('https');
-
-		var req = http.request(options, function(res){
+		let req = http.request(options, function (res) {
 			res.setEncoding('utf8');
-			var data = '';
-		    res.on('data', function (chunk) {
-		        data += chunk;
-		    });
-		    res.on('end', function(){
-				var dataa = JSON.parse(data).data;
-		        if(typeof(callback) == 'function')
-		        	callback(dataa);
+			let data = '';
+			res.on('data', function (chunk) {
+				data += chunk;
+			});
+			res.on('end', function () {
+				data = JSON.parse(data).data;
+				if (typeof (callback) == 'function') {
+					callback(data);
+				}
 			});
 		});
 
-		if(body != '')
-			req.write(querystring.stringify(body));
+		if (body !== '')
+			req.write(body);
 		req.end();
 
 	}
 
-return {
-		get: function get(url, callback){
+	return {
+		get: function get(url, callback) {
 			call('GET', url, '', callback);
 		},
-		post: function post(url, body, callback){
+		post: function post(url, body, callback) {
 			call('POST', url, body, callback);
 		},
-		put: function put(url, body, callback){
+		put: function put(url, body, callback) {
 			call('PUT', url, body, callback);
 		},
-		del: function del(url, callback){
+		del: function del(url, callback) {
 			call('DELETE', url, '', callback);
 		},
 	}
